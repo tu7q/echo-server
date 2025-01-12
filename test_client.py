ab3a52bd8de979060f2b2e4ea633524a949135cc
ablth679 <ablth679@student.otago.ac.nz>
2025-01-12 18:46:20 +1300

    utf client instead

--- test_client.py
+++ test_client.py
@@ -7,7 +7,8 @@ time.sleep(0.5)
 
 while True:
     msg = input()
-    clientsocket.send(bytes(ascii(msg), encoding="ascii"))
-    print(f"recv: {clientsocket.recv(1024).decode("ascii")}")
+
+    clientsocket.send(msg.encode("utf-8"))
+    print(f"recv: {clientsocket.recv(1024).decode("utf-8")}")
 
 clientsocket.close()
